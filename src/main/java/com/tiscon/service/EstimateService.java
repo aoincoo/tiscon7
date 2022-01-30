42fce1516f0f9d7fae291818e164536e4fa4eb1f
nobuhitokimura <denosayuki.ms000@gmail.com>
2022-01-30 11:54:28 +0900

    [Fix] 総段ボール数が200を超えたときにでるエラーを解消

--- src/main/java/com/tiscon/service/EstimateService.java
+++ src/main/java/com/tiscon/service/EstimateService.java
@@ -83,7 +83,7 @@ public class EstimateService {
                 + getBoxForPackage(dto.getWashingMachine(), PackageType.WASHING_MACHINE);
 
         // 箱に応じてトラックの種類が変わり、それに応じて料金が変わるためトラック料金を算出する。
-        int pricePerTruck = estimateDAO.getPricePerTruck(boxes);
+        int pricePerTruck = estimateDAO.getPriceTruck(boxes);
 
         // オプションサービスの料金を算出する。
         int priceForOptionalService = 0;
